2c71f99b29d1a2d2e500cc0f573ae228675ee3b6
Hikaru Misaki <misaki.811033@gmail.com>
2022-10-19 02:47:07 +0000

    Update 6 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import "./styles.css";
+/*
+ * const, let 等の変数宣言
+ *
+ */
+var val1 = "var変数";
+console.log(var1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+//var can be updated
+val1 = "var is updated";
+console.log(var1);
+
